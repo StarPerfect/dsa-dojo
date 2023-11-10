3470151bd3ed88e7e260de701c88482f6472c17e
Corina <starperfect@gmail.com>
2023-11-09 17:55:49 -0700

    Utilize new Node class to create Stack#push and #count

--- stacks/lib/stack.rb
+++ stacks/lib/stack.rb
@@ -1,19 +1,41 @@
 class Stack
-
     def initialize
-        @stack = []
+        @head = nil
     end
 
-    def push(element)
-        @stack.push(element)
+    def push(data)
+         if @head == nil
+            @head = Node.new(data)
+        else
+            current_node = @head
+            new_node = Node.new(data)
+            while current_node.next != nil
+                current_node = current_node.next
+            end
+            current_node.next = new_node
+        end
     end
-
-    def pop
-        @stack.pop
-    end
-
+    
     def count
-        @stack.count
+        if @head == nil
+            count = 0
+        else
+            count = 1
+            current_node = @head
+            while current_node.next != nil
+                count += 1
+                current_node = current_node.next
+            end
+        end
+        count
     end
+end
 
+class Node
+    attr_accessor :data, :next
+    
+    def initialize(data, next_node = nil)
+        @data = data
+        @next = next_node 
+    end
 end
